3900fa62e7e86a54a24658e5483de0c5e23ec865
Francisco Madeira <xico2k@gmail.com>
2020-03-07 15:11:20 +0000

    Minor es6 tweak

--- components/DisableBodyScrollingView.native.js
+++ components/DisableBodyScrollingView.native.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export default (props) => <View {...props} style={[{flex: 1}, props.style]} />
+export default ({ style, ...props }) => <View {...props} style={[{flex: 1}, style]} />
